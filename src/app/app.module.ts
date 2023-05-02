e704f5156c312397dcec59d2eeae0827ff342bde
Micael Bittencourt <micaelbittencourt.dev@gmail.com>
2023-05-02 12:06:08 -0300

    Inicio do menu e to-do list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgMaterialModule } from './ng-material/ng-material.module';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { HeaderComponent } from './todo-list/components/header/header.component';
+import { TodoInputAddItensComponent } from './todo-list/components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoButtonDeleteAllComponent } from './todo-list/components/todo-button-delete-all/todo-button-delete-all.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodoListComponent,
+    HeaderComponent,
+    TodoInputAddItensComponent,
+    TodoButtonDeleteAllComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ng-material/ng-material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  exports: [
+    MatCardModule,
+    MatIconModule
+  ]
+})
+export class NgMaterialModule { }
